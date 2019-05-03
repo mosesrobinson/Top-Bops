b16b92feb05bf041c723c2a98983173fc3058b71
Moses Robinson <mosesjrobinson@icloud.com>
2019-05-03 04:17:35 -0700

    fetch photo operation completed. Implementing Operation in tableview controller

--- TopBops/TopBops/Operations/FetchPhotoOperation.swift
+++ TopBops/TopBops/Operations/FetchPhotoOperation.swift
@@ -6,4 +6,57 @@
 //  Copyright © 2019 Moses Robinson. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class FetchPhotoOperation: ConcurrentOperation {
+    
+    init(album: Album, session: URLSession = URLSession.shared) {
+        self.album = album
+        self.session = session
+        super.init()
+    }
+    
+    override func start() {
+        state = .isExecuting
+        
+        let url = album.artworkURL
+        
+        let task = session.dataTask(with: url) { (data, _, error) in
+            
+            defer { self.state = .isFinished }
+            if self.isCancelled { return }
+            
+            if let error = error {
+                NSLog("Error fetching data for \(self.album): \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data found")
+                return
+            }
+            
+            guard let image = UIImage(data: data) else {
+                NSLog("Could not construct image from data")
+                return
+            }
+            
+            self.image = image
+        }
+        task.resume()
+        dataTask = task
+    }
+    
+    override func cancel() {
+        super.cancel()
+        dataTask?.cancel()
+    }
+    
+    //MARK: - Properties
+    
+    let album: Album
+    private let session: URLSession
+    private(set) var image: UIImage?
+    private var dataTask: URLSessionDataTask?
+}
+
