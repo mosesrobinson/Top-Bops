fee5d4ae9de826751f36cdfb7b41443ceba4dd8e
Moses Robinson <mosesjrobinson@icloud.com>
2019-05-03 00:28:10 -0700

    Application now has model matching needed data from RSS Feed.

--- TopBops/TopBops/Model/Album.swift
+++ TopBops/TopBops/Model/Album.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+struct Album: Codable {
+    
+    let albumName: String
+    let artistName: String
+    let genres: [Genre]
+    let releaseDate: String
+    let copyright: String
+    let artworkURL: URL
+    let itunesURL: URL
+    
+    struct Genre: Codable {
+        let name: String
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case albumName = "name"
+        case artistName
+        case genres
+        case releaseDate
+        case copyright
+        case artworkURL = "artworkUrl100"
+        case itunesURL = "url"
+    }
+}
+
+struct Albums: Codable {
+    let results: [Album]
+}
