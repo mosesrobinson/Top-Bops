a1f45e3e7f802db51bb4f3c268699682e8f83367
Moses Robinson <mosesjrobinson@icloud.com>
2019-05-03 01:08:56 -0700

    Application has implementation to fetch for top ten albums from feed.

--- TopBops/TopBops/Networking/AlbumController.swift
+++ TopBops/TopBops/Networking/AlbumController.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+
+class AlbumController {
+    
+    init() {
+        fetchTopTenAlbums { (error) in
+            
+            if let error = error {
+                NSLog("Unable to fetch albums from RSS Feed: \(error)")
+                return
+            }
+        }
+    }
+    
+    private func fetchTopTenAlbums(completion: @escaping (Error?) -> Void) {
+        
+        let url = baseURL.appendingPathComponent("explicit").appendingPathExtension("json")
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, _, error) in
+            
+            if let error = error {
+                NSLog("Error retrieving JSON: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from data task.")
+                completion(NSError())
+                return
+            }
+            
+            do {
+                let jsonDecoder = JSONDecoder()
+                let decodedAlbums = try jsonDecoder.decode(Albums.self, from: data).results
+                
+                self.albums = decodedAlbums
+                completion(nil)
+            } catch {
+                NSLog("Error decoding data.")
+                completion(error)
+                return
+            }
+            
+        }
+        dataTask.resume()
+    }
+    
+    //MARK: - Properties
+    
+    private let baseURL = URL(string: "https://rss.itunes.apple.com/api/v1/us/itunes-music/top-albums/all/10")!
+    private(set) var albums: [Album] = []
+    
+}
